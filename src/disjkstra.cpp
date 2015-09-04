8c28e8667c17212bee4117d3eca3f6cfc008b0d9
Carlos Laguna <claguna@oracle.(none)>
2015-09-04 17:32:15 -0500

    dijsktra

---
+++ src/disjkstra.cpp
@@ -0,0 +1,38 @@
+/* https://www.hackerrank.com/challenges/dijkstrashortreach */
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <utility> 
+#include <limits>
+
+
+int main()
+{
+  int testcases;
+  int number_nodes, number_edges;
+  
+  for(int i=0; i < testcases; i++ ){
+    int x, y, r;
+    scanf("%d %d", &number_nodes, &number_edges);
+    for(int j=0; j < number_edges; j++){
+      /*node_x, node_y, weight*/
+      scanf("%d %d %d", &x, &y, &r);
+      
+    }
+  }
+  
+  return 0;
+}
